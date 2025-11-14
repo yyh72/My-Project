67b4641712ae22ce24cef6b0a68cd8ec46d014ae
yyh72 <1804186474@qq.com>
2025-11-14 21:53:40 +0800

    完善排序接口，添加打印代码

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 void mysort(int arr[],int len)
 {
-	
+	for(int i=len-1;i>0;i--)
+	{
+		for(int j=0;j<i;j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
 }
 
 int main()
@@ -12,6 +23,12 @@ int main()
 	int len = sizeof(arr)/sizeof(arr[0]);
 	
 	mysort(arr,len);
+	
+	for(auto x : arr)
+	{
+		cout << x << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
